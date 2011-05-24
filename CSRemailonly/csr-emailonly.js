ff049726206a6a811ef42cee0620062888a28a5c
Skip Linux <skipjack@skipjack.info>
2011-05-24 07:16:33 -0700

    Changed the message for expired token

--- CSRemailonly/csr-emailonly.js
+++ CSRemailonly/csr-emailonly.js
@@ -3,4 +3,10 @@ $j('#top').show();
 $j('#footer').show();
 $j('#sidebar').show();
 $j('#contentwrapper').css("padding","20px");
+$j(".content_grey:contains('No such token')").html(
+
+'<div class="grey_box_top"><div class="box box_top"></div></div> <p> No such token: The ticket may have been rated already or the token has expired </p> <p> </p> <div class="grey_box_bottom"><div class="box box_bottom"></div></div>'
+
+);
+
 }
