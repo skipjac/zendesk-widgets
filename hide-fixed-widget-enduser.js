5d2837be39a669364e7cf3b86fb6cbd7ef344b2b
Skip Moore <skipjack@skipjack.info>
2011-12-15 12:33:34 -0800

    adding hide fixed widgets on submit page

---
+++ hide-fixed-widget-enduser.js
@@ -0,0 +1,10 @@
+(function() {
+
+    $j(document).ready(function() {  
+
+       //check to see if you are on the end users request page
+       if(location.pathname === '/requests/new' || location.pathname === '/anonymous_requests/new') {
+           j$('div#widget_fixed').hide();
+       }
+    });
+}());
