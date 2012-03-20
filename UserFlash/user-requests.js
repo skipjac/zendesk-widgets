fd217026b9896c063f84a4175710face2a70488f
Skip Moore <skip@skipjack.info>
2012-03-20 13:50:20 -0700

    added user lang selectable messages

--- UserFlash/user-requests.js
+++ UserFlash/user-requests.js
@@ -1,10 +1,18 @@
 (function($) {
+	//place the message you want for each language here
+	langMap = {
+		1: "something in english",
+		2: "something in spanish",
+		16: "something in french",
+		19: "something in portugese"
+		
+	}
+	
      hideDiv = function(){
          $("ul#banners li.skip").css("display", "none");
      }
      var HasTickets = function() {
 
-        
              $.getJSON('/api/v1/requests.json', function(searchResults) {
                  var x = searchResults.length;
                  if (x === 0) {
@@ -24,8 +32,18 @@
 
      var checkDupeTix2 = function(xLength) {
 
-              hideDiv();
-             $("ul#banners").append( '<li class="skip"><span class="icon">&nbsp; &nbsp; &nbsp; </span><span class="content">Please check your email address.</span><span class="ignore">(<a href="#" OnClick="hideDiv();" >ignore this</a>)</span><span class="reload"><a href="">reload</a></span></li>');
+	  		$.getJSON('/users/current.json', function(data) {
+				 hideDiv();
+				 if (langMap[data.locale_id] == null) {
+					 uLang = langMap[1];
+				 } else {
+				 	uLang = langMap[data.locale_id];
+				 }
+	  			
+	  			$("ul#banners").append( '<li class="skip"><span class="icon">&nbsp; &nbsp; &nbsp; </span><span class="content">' +  uLang +'.</span><span class="ignore">(<a href="#" OnClick="hideDiv();" >ignore this</a>)</span><span class="reload"><a href="">reload</a></span></li>');
+	  		})
+			  
+             
 
      }
 
