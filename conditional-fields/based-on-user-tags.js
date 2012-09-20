5546ac3eca8c45018fa14f3a35eeff8aeb48dcdf
skipjac <skip@skipjack.info>
2012-09-20 11:18:52 -0700

    updated the notes on user based conditional fields

--- conditional-fields/based-on-user-tags.js
+++ conditional-fields/based-on-user-tags.js
@@ -1,7 +1,7 @@
 (function () {
-    /*create the various options you want to display 
-	**each array needs to be built out with even number of members
-	**in a tag, title format. 
+  /* create the various options you want to display 
+	** each array needs to be built out with even number of members
+	** in a tag, title format. All options in the array must exist in the ticket fields settings. 
 	*/
   var option1 = ['alpha','alpha1','angery_blue','Angery/blue'];
 	var option2 = ['beepling_computer','Beepling Computer','depressed','depressed'];
@@ -28,23 +28,24 @@
 	        }
 	    }
 
-        //default options when page 1st loads 
+      //default options when page 1st loads. you can remove this if you do not have any default options
 	    $j("select#ticket_fields_104609").html(Buildoptions.clear());
 	    makeSelection('select#ticket_fields_104609',option3);
 	    //watch the root field for changes 
-	    $j(document).ready(function() {  
-            //grab the value of the dropdown
-            var userTags = currentUser.tags;
-		    if( _.include(userTags, 3) ) { 
+	    $j(document).ready(function() {
+        //get the array of user tags
+        var userTags = currentUser.tags;
+        //search to see which tags are in the user tags and add the menu options built out from the arrays 
+		    if( _.include(userTags, 'option2') ) { 
 			    makeSelection('select#ticket_fields_104609',option2);
 		    } 
 		    if ( _.include(userTags, 'option1') ) { 
 			    makeSelection('select#ticket_fields_104609',option1);
 		    } 
-		    if ( _.include(userTags, 3) ){
+		    if ( _.include(userTags, 'option3') ){
 		        makeSelection('select#ticket_fields_104609',option3);
 		    }
-		    if ( _.include(userTags, 3) ){
+		    if ( _.include(userTags, 'fieldOption1') ){
 		        makeSelection('select#ticket_fields_104609',option1);
 		    }
 	    });
